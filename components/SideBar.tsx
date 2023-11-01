ec081c9208ae1e99f77f80c762d2a581b81ee8ec
Yakin Teshome <yakin16tm@gmail.com>
2023-11-01 22:50:59 +0300

    fix: asset import path

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import logo from '../logo.svg'
+import logo from "@/assets/images/logo.png";
 import Card from './Card'
 import SideBarLink from './SibeBarLink'
 const links = [
